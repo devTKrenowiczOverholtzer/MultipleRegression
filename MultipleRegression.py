e79517efa8be76f05d89f4c1c612a6eff9d5e565
devtkrenowiczoverholtzer <devtkrenowiczoverholtzer@gmail.com>
2024-09-19 09:30:53 -0700

    intialized dataframes

--- MultipleRegression.py
+++ MultipleRegression.py
@@ -9,4 +9,10 @@ from sklearn.model_selection import train_test_split
 
 # Load in dataset 
 # Make wine dataframe which is the read csv file , has weird semicolon seperators specify with sep
-wine_df = pd.read_csv('winequality-white.csv', sep =';')
+wine_df = pd.read_csv('winequality-white2.csv', sep =';')
+# create a list of features we want to use in our regression 
+regression_list = ["alcohol", "pH","sulphates"]
+output_variable = wine_df["quality"]
+# regression dataframe is our wine dataframe but restricted to the regression list 
+regression_df = wine_df[regression_list]
+
